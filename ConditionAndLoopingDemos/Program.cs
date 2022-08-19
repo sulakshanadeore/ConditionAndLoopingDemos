63250dbfe177391d202c99d6c86bd2eec65f7dfe
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-19 10:30:35 +0530

    completed if

--- ConditionAndLoopingDemos/Program.cs
+++ ConditionAndLoopingDemos/Program.cs
@@ -9,16 +9,19 @@ namespace ConditionAndLoopingDemos
         {
             //This is a comment.
 
+
+
+            #region If-ElseRegion
             int i = 10;
             Console.WriteLine("Enter the value for i");
             i = Convert.ToInt32(Console.ReadLine());
-            if (i!=0)
+            if (i != 0)
             {
-                if (i>0 && i<10)
+                if (i > 0 && i < 10)
                 {
                     Console.WriteLine("I is greater than 0 and smaller than 10");
                 }
-                else if(i==10)
+                else if (i == 10)
                 {
                     Console.WriteLine("I is equal to 10");
                 }
@@ -26,12 +29,13 @@ namespace ConditionAndLoopingDemos
                 {
                     Console.WriteLine("None from the options");
                 }
-                
+
             }
             else
             {
                 Console.WriteLine("I is not equal to 10");
-            }
+            } 
+            #endregion
         }
     }
 }
