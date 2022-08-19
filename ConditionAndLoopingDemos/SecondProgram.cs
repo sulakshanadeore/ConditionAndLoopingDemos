06aff8f46feb0b0a3e5e758d9f5489320e1ac77e
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-19 10:44:40 +0530

    If not example

--- ConditionAndLoopingDemos/SecondProgram.cs
+++ ConditionAndLoopingDemos/SecondProgram.cs
@@ -11,6 +11,17 @@ namespace ConditionAndLoopingDemos
         static void Main(string[] args)
         {
             Console.WriteLine("This is my second main");
+            string s = "Hello ";
+            string name = String.Empty;
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            if (!string.IsNullOrEmpty(name))
+            {
+                string greet = string.Concat(s, name);
+                //string greet = s + name;
+                Console.WriteLine(greet);
+            }
+
             Console.ReadLine();
         }
     }
