8263d30dc46dc07b57f6b6a24751c5c2176a9697
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-19 09:31:23 +0530

    first change

--- ConditionAndLoopingDemos/Program.cs
+++ ConditionAndLoopingDemos/Program.cs
@@ -7,7 +7,11 @@ namespace ConditionAndLoopingDemos
     {
         static void Main(string[] args)
         {
-             
+            int i = 10;
+            if (i==10)
+            {
+                Console.WriteLine("I is equal to 10");
+            }
         }
     }
 }
