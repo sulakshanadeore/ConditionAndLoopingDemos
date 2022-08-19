44230849f76607c83c98c99778dec7660d7d5b1a
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-19 09:43:57 +0530

    Nested If Change

--- ConditionAndLoopingDemos/Program.cs
+++ ConditionAndLoopingDemos/Program.cs
@@ -8,10 +8,25 @@ namespace ConditionAndLoopingDemos
         static void Main(string[] args)
         {
             //This is a comment.
+
             int i = 10;
-            if (i==10)
+            Console.WriteLine("Enter the value for i");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (i!=0)
             {
-                Console.WriteLine("I is equal to 10");
+                if (i>0 && i<10)
+                {
+                    Console.WriteLine("I is greater than 0 and smaller than 10");
+                }
+                else if(i==10)
+                {
+                    Console.WriteLine("I is equal to 10");
+                }
+                else
+                {
+                    Console.WriteLine("None from the options");
+                }
+                
             }
             else
             {
